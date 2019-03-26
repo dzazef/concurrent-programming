a2b495823fb600ba405b2b3b6098e529e696caf2
dzazef <dzazef@gmail.com>
2019-03-26 23:39:43 +0100

    Finish task1 implementation in Ada

--- enterprise-simulator/ada/parameters.ads
+++ enterprise-simulator/ada/parameters.ads
@@ -1,11 +1,11 @@
 package Parameters is
     Start_Delay             : constant := 2000;
-    Max_Tasks               : constant := 100;
-    Storage_Capacity        : constant := 100;
+    Max_Tasks               : constant := 60;
+    Storage_Capacity        : constant := 60;
     Ceo_Speed               : constant := 0.1;
-    Worker_Speed            : constant := 2.0;
-    Client_Speed            : constant := 3.0;
-    Workers                 : constant := 2;
-    Clients                 : constant := 1;
+    Worker_Speed            : constant := 10.0;
+    Client_Speed            : constant := 10.0;
+    Workers                 : constant := 20;
+    Clients                 : constant := 20;
     Max_Arguments           : constant := 1000;
 end Parameters;
